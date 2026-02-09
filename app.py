2403875b791e2e656dcf8648fe50000991d23003
Dydovič Tomáš <dydovic@fotbal.cz>
2026-02-09 15:44:15 +0100

    Upgrade z VS

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import socket
 
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        msg = f"Hello app Změna v2 Host={socket.gethostname()}\n"
+        msg = f"Hello app Zmena v3 Host={socket.gethostname()}\n"
         self.send_response(200)
         self.send_header("Content-Type", "text/plain")
         self.end_headers()
