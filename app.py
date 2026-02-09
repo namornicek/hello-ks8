5c96fb0f6de197cd9405615f091e1655574b189e
dyda <test@lab.local>
2026-02-09 13:23:32 +0000

    Add GitHub Actions pipeline

---
+++ app.py
@@ -0,0 +1,12 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import socket
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        msg = f"Hello from CI/CD! Host={socket.gethostname()}\n"
+        self.send_response(200)
+        self.send_header("Content-Type", "text/plain")
+        self.end_headers()
+        self.wfile.write(msg.encode())
+
+HTTPServer(("0.0.0.0", 8080), Handler).serve_forever()
