9bf03a57caa43892eec11cacd108cc215921cccf
Dydovič Tomáš <dydovic@fotbal.cz>
2026-02-10 08:47:36 +0100

    Upgrade V_8

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import socket
 
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        msg = f"Hello app Zmena V_5 Host={socket.gethostname()}\n co treba druhy radek \n treti radek \n ètvrty radel"
+        msg = f"Hello app Zmena V_8 Host={socket.gethostname()}\n Verze 8"
         self.send_response(200)
         self.send_header("Content-Type", "text/plain")
         self.end_headers()
